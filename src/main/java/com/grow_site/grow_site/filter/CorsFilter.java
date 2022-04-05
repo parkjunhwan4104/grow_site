b145e795547209a324e062ac79d727ec21bd71a1
parkjunhwan <wnsghks4104@gmail.com>
2022-04-06 06:50:30 +0900

    미디어 화면 크기 별 이미지 크기 설정

--- src/main/java/com/grow_site/grow_site/filter/CorsFilter.java
+++ src/main/java/com/grow_site/grow_site/filter/CorsFilter.java
@@ -5,6 +5,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -16,7 +17,6 @@ public class CorsFilter implements Filter {
     public void init(FilterConfig filterConfig) throws ServletException {
 
     }
-
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
@@ -37,7 +37,6 @@ public class CorsFilter implements Filter {
             chain.doFilter(req, res);
         }
     }
-
     @Override
     public void destroy() {
 
